61484443ba810565528823fc9809e6a812c18f76
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-01-07 00:45:06 +0600

    Initial commit

---
+++ src/VApp.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VApp",
+});
+</script>
+
+<template>
+  <main class="flex-fill">
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <header>
+              <h1>Hello World</h1>
+            </header>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import VApp from "./VApp.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+createApp(VApp).mount("#app");
